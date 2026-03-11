16b10d387e32b3e47fbcb6220a5e04681e864ae1
A$AP vorp <126407220+Marius-Poirier@users.noreply.github.com>
2026-03-11 12:33:42 +0100

    Delete backend directory

--- backend/src/routes/public.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router = Router()
-// GET /api/public
-    router.get('/', (_req, res) => {
-    res.json({ message: 'Bienvenue sur l’API publique (accès libre)' })
-})
-
-export default router
